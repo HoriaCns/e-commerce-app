cd119663004107b443cc83010143cfe6cc54799e
HoriaCns <horiacns@protonmail.com>
2025-06-24 17:17:13 +0100

    Added functionality for pages and buttons

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:side_app/components/bottom_nav_bar.dart';
+import 'package:side_app/pages/cart_page.dart';
+import 'package:side_app/pages/shop_page.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update our selected index
+  // when the user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
